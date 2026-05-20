7b5c595161414fa9d8b41be422172cc719424df6
ben <byujisuzuki@gmail.com>
2026-05-20 16:01:18 -0300

    feat: Registrar módulo optimization em models/mod.rs

--- fuzzysimulated/server/src/models/mod.rs
+++ fuzzysimulated/server/src/models/mod.rs
@@ -4,11 +4,13 @@ pub mod term;
 pub mod rule;
 pub mod simulation;
 pub mod batch;
+pub mod optimization;
 
 pub use system::*;
 pub use variable::*;
 pub use term::*;
 pub use rule::*;
 pub use simulation::*;
+pub use optimization::*;
 #[allow(unused_imports)]
 pub use batch::*;
