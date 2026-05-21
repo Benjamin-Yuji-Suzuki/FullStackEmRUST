9fc1cf77fb6078d0279ed94fddb93f7ae48c486a
ben <byujisuzuki@gmail.com>
2026-05-21 15:08:54 -0300

    test: configurar baseURL e webServer do Playwright

--- fuzzysimulated/end2end/playwright.config.ts
+++ fuzzysimulated/end2end/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -97,8 +97,10 @@ export default defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'cargo leptos watch',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: true,
+  },
 });
